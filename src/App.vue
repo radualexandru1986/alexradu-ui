04a0b642a724b2e32b652c803c33d7ff1143aee1
radualexandru1986 <radu.c.alexandru@gmail.com>
2022-03-20 21:01:09 +0000

    build

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-lg-6">
+        <h1 class="display-2 text-center text-xl-start"> Tweets </h1>
+      </div>
+      <div class="col-12 col-lg-6">
+        <div class="form-group h-100 d-flex justify-content-center align-items-center">
+          <input type="text"
+                 v-model="searchTerm"
+                 class="form-control border-2 border-dark form-control-lg text-center text-xl-start"
+                 placeholder="Search in tweets">
+        </div>
+      </div>
+    </div>
+
+    <hr>
+    <p class="text-muted text-center text-xl-start w-100"> @FarrowsDesign - Last 20 tweets </p>
+    <p class="text-muted text-center text-xl-start w-100" v-if="searchTerm">Matching results : {{this.tweets.length}}</p>
+    <div class="row mt-5 p-1">
+      <CardComponent v-for="(tweet, index) in tweets" v-bind:key=index v-bind:details = tweet> </CardComponent>
+    </div>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CardComponent from './components/Card';
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CardComponent
+  },
+  data(){
+    return {
+      serverResponse:[],
+      searchTerm:'',
+
+    }
+  },
+  methods:{
+
+  },
+  computed :{
+    tweets(){
+      return this.serverResponse.filter(tweet=>{
+          return tweet.text.toLowerCase().match(this.searchTerm.toLowerCase())
+        })
+    }
+  },
+  mounted() {
+    axios.get('https://backend.alexradu.co.uk/api/tweets').then(response=>{
+      console.log(response.data)
+      this.serverResponse = response.data
+    })
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import '~bootstrap/dist/css/bootstrap.css';
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="col-12 col-md-6">
+    <div class="card mt-2">
+      <div class="card-body">
+        <h5 class="card-title"> {{details.created_at}}  </h5>
+        <h6 class="card-subtitle mb-2 text-muted">@{{JSON.parse(details.user).screen_name}}</h6>
+        <div class="row d-inline">
+          <span><strong>{{JSON.parse(details.user).location}}</strong></span>
+          <span><strong>{{JSON.parse(details.user).followers_count}}</strong> Followers</span>
+          <span><strong>{{JSON.parse(details.user).friends_count}}</strong> Friends</span>
+        </div>
+        <div class="row mt-2">
+          <p class="card-text"> {{details.text}} </p>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props : {
+    details : Object
+  },
+  name: "CardComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
